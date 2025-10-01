8daef62e74c52d2d80229688f62a05b495026b2a
yc3697 <youngchan9683@gmail.com>
2025-10-01 14:04:46 +0900

    build: test 관련 빌드 수정 및 application 환경 분리

--- src/test/java/com/capstone/team07/Team07ApplicationTests.java
+++ src/test/java/com/capstone/team07/Team07ApplicationTests.java
@@ -2,8 +2,10 @@ package com.capstone.team07;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Team07ApplicationTests {
 
 	@Test
