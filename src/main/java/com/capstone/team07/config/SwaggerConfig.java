0bee9bfae6fd8b5b3fd6ec0d56f0b58bce11b233
yc3697 <youngchan9683@gmail.com>
2025-10-01 14:17:33 +0900

    build: swagger 세팅

---
+++ src/main/java/com/capstone/team07/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.capstone.team07.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Profile("!test")
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
+    // 공통 OpenAPI - 관리자와 사용자 모두 볼 수 있음
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("캡스톤 API")
+                .description("Team07 백엔드 API 입니다.")
+                .version("1.0");
+
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(new Server().url(serverUrl)));
+    }
+
+}
