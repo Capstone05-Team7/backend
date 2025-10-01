dbbe22f4e29a938f7954efaddbf3e79c7c59e02c
yc3697 <youngchan9683@gmail.com>
2025-10-01 10:55:37 +0900

    init: spring boot 초기 설정

---
+++ src/test/java/com/capstone/team07/Team07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.team07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
